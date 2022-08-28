3a41cfda42c7f7a1ac3f402def52a8ea1df6fdce
Jacob maximus Usaraga <jlusaraga@up.edu.ph>
2022-08-29 06:42:17 +0800

    added styling for the sidebar

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Discord-Like Project(React-Tailwind)
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app) and TailWind CSS. This project was a clone of Fireship's [Ultimate TailWind CSS Tutorial](https://www.youtube.com/watch?v=pfaSUYaSgRo) with a little bit of modification as a way to understand Tailwind CSS.
+
 In this project, I used the Vergil background with a better discord theme.
+
+
 To run the project on your computer, clone the repo and run `npm install` on the cli on the correct directory. Afterwards, run `npm start` to run the development server.
--- src/SideBar.js
+++ src/SideBar.js
@@ -1,19 +1,48 @@
 import {BsPlus, BsFillLightningFill, BsGearFill} from 'react-icons/bs';
-import {FaFire, FaPoo} from 'react-icons/fa';
+import {FaDiscord, FaPoo} from 'react-icons/fa';
+import { useState, useEffect, useRef } from 'react';
+const SideBar = (props) => {
+    const iconsId = [
+        "discord-icon",
+        "poo-icon",
+        "lightning-icon",
+        "add-icon"
+    ]
+    const [activeElement, setActiveElement] = useState("discord-icon");
+    function updateActiveElement(iconId){
+        setActiveElement(iconId);
+    }
+    useEffect(() => {
+        console.log(activeElement);
+        for(let i = 0; i < iconsId.length; i++){
+            document.getElementById(iconsId[i]).classList.remove("active");
+        }
+        if(document.querySelector(`#${activeElement}`)!==null){
+        document.querySelector(`#${activeElement}`).classList.add("active");
+        }
+    });
 
-const SideBar = () => {
 return(
-    <div className = "fixed top-0 left-0 h-screen w-16 m-0 flex flex-col shadow bg-gray-900 text-white">
-        <SideBarIcon icon = {<FaFire size = "28" />}  text = {"fire"} />
-        <SideBarIcon icon = {<BsPlus size = "32" />} text = {"add"}/>
-        <SideBarIcon icon = {<BsFillLightningFill size = "20" />} text = "fill"/>
-        <SideBarIcon icon = {<FaPoo size = "28" />} text = "poo"/>
+    <div className = "fixed top-0 left-0 h-screen w-16 m-0 flex flex-col shadow bg-opacity-0 text-white">
+        <SideBarIcon icon = {<FaDiscord size = "28" color = "white" />}  text = {"Direct Messages"} onClick={() => updateActiveElement("discord-icon")} id = "discord-icon"/>
+            <button className = "public "> public
+        </button>
+        <div className = "mt-2 mb-4 mx-4">
+        <hr className='border-slate-300'></hr>
+
+        </div>
+        <SideBarIcon icon = {<BsPlus size = "32" />} text = {"add"} onClick={() => updateActiveElement("add-icon")} id = "add-icon"/>
+        <SideBarIcon icon = {<BsFillLightningFill size = "20" />} text = "fill" id = "lightning-icon" onClick={() => updateActiveElement("lightning-icon")}/>
+        <SideBarIcon icon = {<FaPoo size = "28" />} text = "poo" id = "poo-icon" onClick={() => updateActiveElement("poo-icon")}/>
     </div>
 )
 }
-const SideBarIcon = ({icon, text}) => {
+const SideBarIcon = ({icon, text,id, active, onClick}) => {
     return(
-    <div className="sidebar-icon group">
+    <div className="sidebar-icon group"  onClick={onClick} id ={id} >
+        <div className="sidebar-icon-style">
+
+        </div>
         {icon}
         <span className = "sidebar-tooltip group-hover:scale-100">
             {text}
