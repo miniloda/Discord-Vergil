333799a4355f2a252a5340ff078a5fb96c5a5ee3
Jacob maximus Usaraga <jlusaraga@up.edu.ph>
2022-08-31 03:36:20 +0800

    removed text loading

--- src/DiscordLoading.js
+++ src/DiscordLoading.js
@@ -1,19 +1,25 @@
 import "./index.css"
-
+import { useEffect } from "react";
 
 const DiscordLoading = () => {
+    // useEffect(() =>{
+    //     const handleResize  = () => {
+    //         document.querySelector("#spinning-logo").style.width = "80% !important";
+    //         document.querySelector("#spinning-logo").style.height = "auto";
+    //     }
+    //     window.addEventListener("resize", handleResize)
+
+    //     return () => {
+    //         window.removeEventListener("resize", handleResize)
+    //     }
+    // })
     return (
         <div className="flex h-screen w-screen justify-center items-center bg-gray-900">
             <div className = "relative w-fit h-auto">
-            <img src = {require("./assets/spin-discord-logo.gif")}>
+            <img src = {require("./assets/spin-discord-logo.gif")} id= "spinning-logo">
 
             </img>
             <div className = "absolute w-full h-40 flex justify-center items-start bottom-0 left-0">
-            <h1 className = "text-white text-xl">
-                Loading...
-
-            </h1>
-
             </div>
             </div>
         </div>
